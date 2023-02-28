cb609e74edbfcafa2a4be55b0eacfb04dd039061
Kostiantyn Kotenko <90977909+Konstantin-Kotenko@users.noreply.github.com>
2023-02-28 17:49:50 +0200

    add component button and writed props of button

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+    <button v-bind:type='type'>
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    export default {
+        // eslint-disable-next-line vue/multi-word-component-names
+        name:'Button',
+        props:{
+            type:{
+                type:String,
+                default:'button'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
