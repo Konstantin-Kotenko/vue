7d55acbfc5c4276e6d578ae23e6f805ffce38a61
Kostiantyn Kotenko <90977909+Konstantin-Kotenko@users.noreply.github.com>
2023-03-06 20:56:33 +0200

    edit project, practice with v-if, v-show

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
 <div id='app'>
   <h1>{{ title }}</h1>
 <Button @click="increment" outlined>Click me</Button>
+<DirectivesTest/>
 </div>
   </template>
 
 <script>
 import Button from './components/Button.vue'
+import DirectivesTest from './components/DirectivesTest.vue'
 
 export default {
   name: 'App',
   components: {
-    Button
+    Button,
+    DirectivesTest
   },
   data(){
     return{
